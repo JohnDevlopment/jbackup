64f2bdd4257549e3f57363fc9f51645a670d937a
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-12-31 15:16:29 -0600

    fixup! Implement rule config adaptor

--- jbackup/tests/test_rules.py
+++ jbackup/tests/test_rules.py
@@ -1,7 +1,8 @@
 from tempfile import TemporaryFile
 
 from ..rules.config.toml_config_adapter import TOMLFile
-from ..rules.config import MissingSectionError, MissingOptionError
+from typing import Any
+from pathlib import Path
 import pytest, io
 
 @pytest.fixture
@@ -14,12 +15,38 @@ src = "/home/user/somedir"
 options.parallel = true
 """
 
-def test_toml_file(toml_string: str) -> None:
-    with TemporaryFile('w+b') as fd:
-        data = toml_string.encode('UTF-8')
-        fd.write(data)
-        fd.seek(0)
-        tomlf = TOMLFile(fd)
+class TestFile:
+    def __init__(self, p: str, mode: str):
+        self.fd: Any = None
+        self.path = Path(p)
+        self.args = (p, mode)
+
+    def __enter__(self):
+        self.fd = open(*self.args)
+        return self
+
+    def __exit__(self, *args):
+        self.fd.close()
+        self.path.unlink()
+
+    @classmethod
+    def create(cls, p: str, mode: str):
+        return cls(p, mode)
+
+@pytest.mark.parametrize('file_is_string', (False, True))
+def test_toml_file(toml_string: str, file_is_string: bool) -> None:
+    if file_is_string:
+        with open('file.toml', 'w') as fd:
+            fd.write(toml_string)
+
+        with TestFile.create('file.toml', 'rb') as f:
+            tomlf = TOMLFile(f.fd)
+    else:
+        with TemporaryFile('w+b') as fd:
+            data = toml_string.encode('UTF-8')
+            fd.write(data)
+            fd.seek(0)
+            tomlf = TOMLFile(fd)
 
     assert '/copy' in tomlf
     assert 'copy/dest' in tomlf
