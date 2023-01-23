33a96ac439ba41b222444303f26f30c5aa78347a
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-01-23 17:12:36 -0600

    tests: actions, Update properties of test action
    Includes changes to the rule file as well

--- src/jbackup/tests/_testaction.py
+++ src/jbackup/tests/_testaction.py
@@ -1,19 +1,27 @@
 # Test action
 
 from __future__ import annotations
-from typing import TYPE_CHECKING, Optional, Any, cast
+from typing import TYPE_CHECKING
 from jbackup.actions import ActionProperty
 
 if TYPE_CHECKING:
     from jbackup.rules import Rule
+    from typing import Optional, Any, cast
 
 class Action_Test:
     properties: list[ActionProperty] = [
-        ActionProperty('message', 'This is a test action')
+        ActionProperty('message', ''),
+        ActionProperty('extra-message', None, optional=True)
     ]
 
     def __init__(self, rule: Rule) -> None:
         self.rule = rule
+        self.propmapping = ActionProperty.get_properties('test', rule, *self.properties)
 
     def run(self) -> None:
-        print("Dummy test")
+        print("== Run action 'test' ==")
+        print(self.propmapping['message'])
+        exmsg: str = self.propmapping.get('extra-message', '')
+        if exmsg:
+            print(exmsg)
+        print("== End of action ==")
