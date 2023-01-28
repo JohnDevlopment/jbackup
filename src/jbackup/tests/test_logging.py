49be566236aa99da7a09ebd7e808083677559e5c
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-01-27 23:13:53 -0600

    tests: logging, captured output at start of test_logger
    Also, add additional info in the asserts.

--- src/jbackup/tests/test_logging.py
+++ src/jbackup/tests/test_logging.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from ..logging import get_logger, Level
 from typing import NamedTuple
 
@@ -8,22 +9,24 @@ class CaptureResult(NamedTuple):
 def test_logger(capsys):
     logger = get_logger('tests.logging', Level.DEBUG)
 
+    capsys.readouterr()
+
     logger.debug("See this message?")
     captured: CaptureResult = capsys.readouterr()
-    assert captured.out.startswith("DEBUG")
+    assert captured.out.startswith("DEBUG"), f"does not start with \"DEBUG\" ({captured.out!r})"
 
     logger.info("See this message?")
     captured: CaptureResult = capsys.readouterr()
-    assert captured.out.startswith("INFO")
+    assert captured.out.startswith("INFO"), f"does not start with \"INFO\" ({captured.out!r})"
 
     logger.warn("See this message?")
     captured: CaptureResult = capsys.readouterr()
-    assert captured.out.startswith("WARN")
+    assert captured.out.startswith("WARN"), f"does not start with \"WARN\" ({captured.out!r})"
 
     logger.error("See this message?")
     captured: CaptureResult = capsys.readouterr()
-    assert captured.out.startswith("ERROR")
+    assert captured.out.startswith("ERROR"), f"does not start with \"ERROR\" ({captured.out!r})"
 
     logger.critical("See this message?")
     captured: CaptureResult = capsys.readouterr()
-    assert captured.out.startswith("CRITICAL")
+    assert captured.out.startswith("CRITICAL"), f"does not start with \"CRITICAL\" ({captured.out!r})"
