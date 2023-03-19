57b5b7f4f626684ba629917deb9dd6a350de69e4
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-03-19 13:38:43 -0500

    template: Changes to _template.py
    * Use Level enum
    * Add PropertyType

--- src/jbackup/template/_template.py
+++ src/jbackup/template/_template.py
@@ -1,15 +1,15 @@
 # Template for Actions
 
 from __future__ import annotations
-from typing import TYPE_CHECKING
-from jbackup.actions import ActionProperty
-from jbackup.logging import get_logger
+from typing import TYPE_CHECKING, cast
+from jbackup.actions import ActionProperty, PropertyType
+from jbackup.logging import get_logger, Level
 from jbackup.utils import get_env
-import logging, sys
+import sys
 
 if TYPE_CHECKING:
     from jbackup.rules import Rule
-    from typing import Optional, Any, cast
+    from typing import Any
 
 class Action_Dummy:
     """An explanation for the action."""
@@ -19,9 +19,8 @@ class Action_Dummy:
     def __init__(self, rule: Rule):
         self.rule = rule
         self.propmapping = ActionProperty.get_properties('dummy', rule, self.properties)
-        self.logger = get_logger('dummy')
-        level = get_env('JBACKUP_LEVEL', logging.INFO, type_=int)
-        self.logger.setLevel(level)
+        level = get_env('JBACKUP_LEVEL', Level.INFO, type_=int)
+        self.logger = get_logger('dummy', cast(Level, level))
 
     def run(self) -> None:
         pass
