647043d20d1d9d9f39a5371339b87f9a0b8a078f
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-08-15 10:05:55 -0500

    [UPDATE] Rules: Make Missing*Error classes derived from MissingError

--- jbackup/rules/exceptions.py
+++ jbackup/rules/exceptions.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
-class MissingSectionError(LookupError):
+class MissingError(LookupError):
+    "Error for 'Missing X' cases."
+
+class MissingSectionError(MissingError):
     """Missing section in a rule file."""
+
+class MissingOptionError(MissingError):
+    """An error raised when an option does not exist."""
 
 class RuleParserError(Exception):
     """An error raised while parsing a rule file."""
-
-class MissingOptionError(LookupError):
-    """An error raised when an option does not exist."""
