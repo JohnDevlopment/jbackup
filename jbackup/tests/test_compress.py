8c59f7499032b0d209d0bc1d8be25f21b3cd1375
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-08-19 15:02:48 -0500

    [UPDATE] Unit test for the recursion function

--- jbackup/tests/test_compress.py
+++ jbackup/tests/test_compress.py
@@ -17,7 +17,7 @@ def mkdir(*dirs: StrPath, base: StrPath, parents: bool=False, exists_ok: bool=Fa
 def test_recursion(tmp_path: Path):
     mkdir("dir1", "dir1/sdir1", "dir2", "dir2/sdir1", base=tmp_path)
 
-    dirs = recurse_directory(tmp_path)
+    dirs = recurse_directory(tmp_path, [])
     match dirs:
         case [d1, d2, d3, d4]:
             assert str(d1).endswith("/dir1/sdir1"), f"'{d4}' does not end with '/dir2/sdir1'"
