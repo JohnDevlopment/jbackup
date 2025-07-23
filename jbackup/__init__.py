003e61a37aef99dcfbdc816cc930ac309838aba7
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-07-23 01:27:07 -0500

    [PROJECT] Bump to version 2.0

--- jbackup/__init__.py
+++ jbackup/__init__.py
@@ -4,10 +4,7 @@ Extendable backup system.
 Environment Variables:
   * JBACKUP_LEVEL = logging level
 """
-
 from __future__ import annotations
-
-APPNAME = "jbackup"
-
-__version__ = "2.0a1"
+APPNAME = 'jbackup'
+__version__ = '2.0'
 __author__ = 'John Russell'
